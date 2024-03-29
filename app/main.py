6a0c0fbd92456107aee038beff860fbd59aab026
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2024-03-29 19:18:57 +0000

    Add top 5 section

--- app/main.py
+++ app/main.py
@@ -22,6 +22,7 @@ def load_data(nrows):
     return data
 
 data = load_data(100000)
+original_data = data
 # Map
 st.header("Where are the most people injured in NYC?")
 injured_people = st.slider("Number of persons injured in vehicle collisions", 0, 19) # 19 turned out to be the max
@@ -64,6 +65,19 @@ hist = np.histogram(filtered["date/time"].dt.minute, bins=60, range=(0, 60))[0] 
 chart_data = pd.DataFrame({"minute": range(60), "crashes": hist}) # Create plot
 fig = px.bar(chart_data, x="minute", y="crashes", hover_data=["minute", "crashes"], height=400) # Beautify plot
 st.write(fig)
+
+# Top 5
+st.header("Top 5 dangerous streets by affected type")
+select = st.selectbox("Affected type of people", ["Pedestrians", "Cyclists", "Motorists"])
+
+if select == "Pedestrians":
+    st.write(original_data.query("injured_pedestrians >= 1")[["on_street_name", "injured_pedestrians"]].sort_values(by=["injured_pedestrians"], ascending= False).dropna(how="any")[:5]) # Query data for injured pedestrians to show the top 5 most dangerous streets for pedestrians
+
+elif select == "Cyclists":
+    st.write(original_data.query("injured_cyclists >= 1")[["on_street_name", "injured_cyclists"]].sort_values(by=["injured_cyclists"], ascending= False).dropna(how="any")[:5]) # Query data for injured cyclists to show the top 5 most dangerous streets for cyclists
+
+else:
+    st.write(original_data.query("injured_motorists >= 1")[["on_street_name", "injured_motorists"]].sort_values(by=["injured_motorists"], ascending= False).dropna(how="any")[:5]) # Query data for injured motorists to show the top 5 most dangerous streets for motorists
 
 # Raw
 if st.checkbox("Show Raw Data", False): # Optionally look over raw data
